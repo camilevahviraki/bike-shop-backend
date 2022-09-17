46e4e9f6211542f1d9a5505db1751da1ad70b699
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-09-17 10:58:44 +0100

    Added Motorcycle model

---
+++ db/migrate/20220917095624_create_motorcycles.rb
@@ -0,0 +1,15 @@
+class CreateMotorcycles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :motorcycles do |t|
+      t.integer :productID
+      t.string :brand
+      t.string :model
+      t.string :year
+      t.string :image
+      t.integer :booking_fee
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
