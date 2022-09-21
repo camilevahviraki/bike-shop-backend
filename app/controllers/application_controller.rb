82aeae1fbb8912bdb2ac711b441bad19eee2a335
David Abol Ger <draker74@gmail.com>
2022-09-21 15:50:54 +0300

    Authorization token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::API
   def encode_token(payload)
     JWT.encode(payload, 'my_s3cr3t')
   end
+
+  def decode_token
+    # Authorization
+
+    auth_header = request.headers['Authorization']
+    return unless auth_header
+      token = auth_header.split[1]
+      begin
+        JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
 end
