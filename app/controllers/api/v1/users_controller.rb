8d5ae9e50a50fe26c0d1779990d1210715c4f8fb
David Abol Ger <draker74@gmail.com>
2022-09-21 15:26:46 +0300

    Login working with user token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,17 @@ class Api::V1::UsersController < ApplicationController
       token = encode_token(user_id: @user.id)
       render json: { user: @user, token: }, status: :ok
     else
+      render json: { error: 'user not created' }, status: :not_acceptable
+    end
+  end
+
+  def login
+    @user = User.find_by(username: user_params[:username])
+
+    if @user&.authenticate(user_params[:password])
+      token = encode_token(user_id: @user.id)
+      render json: { user: @user, token:, notice: 'Welcome To The Quest World' }, status: :ok
+    else
       render json: { error: 'Invalid username or password' }, status: :not_acceptable
     end
   end
