727030951bfe2a52525d4443fef7e03b9dd181b3
David Abol Ger <draker74@gmail.com>
2022-09-26 12:56:37 +0300

    Add image serializer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create(username: "John", password: '12345')
-Motorcycle.create(user_id: 1, brand: "Honda", model: "CBR1000RR", year: "2021", image: "https://www.honda.com.au/content/dam/honda/australia/motorcycles/cbr1000rr/2021/overview/overview-1.jpg", booking_fee: 100)
