dedffaa4a988c7541c3844dc46a6e19c22f9e21b
camilevahviraki <camilevahviraki@gmail.com>
2023-01-15 14:16:18 +0200

    Update url link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.action_mailer.default_url_options = { :host => "https://rails-cah.herokuapp.com/" }
+  config.action_mailer.default_url_options = { :host => "https://bike-shop-backend-r.onrender.com/" }
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
