79ba6d002d1e4749258c4cc03bcc5754eb2a2835
camilevahviraki <camilevahviraki@gmail.com>
2023-01-17 23:57:50 +0200

    Add devise jwt models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+  # has_secure_password
   has_many :reservations, foreign_key: 'user_id', dependent: :delete_all
   has_many :motorcycles, foreign_key: 'user_id', dependent: :delete_all
+
+  has_one_attached :image
 end
